dc5cf8b35ad30a54ece3002660c6a28eff58b5d3
lijie <lijie@lijie-laptop.(none)>
2012-12-09 20:45:49 +0800

    added HiveBPlusRecord

---
+++ src/util/HiveBPlusRecord.java
@@ -0,0 +1,10 @@
+package util;
+
+public class HiveBPlusRecord extends BPlusRecord<String, String>{
+
+	public HiveBPlusRecord(String key, String value) {
+		super(key, value);
+		// TODO Auto-generated constructor stub
+	}
+
+}
