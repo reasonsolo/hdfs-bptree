243467147640b3112a5cc2e62fc2a81b0a57a717
reasonsolo <reasonsolo@gmail.com>
2012-12-09 19:25:10 +0800

    todo: write a new record type

--- src/main/HDFSBTreeTest.java
+++ src/main/HDFSBTreeTest.java
@@ -3,12 +3,16 @@ package main;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.File;
+import java.util.List;
+
 import org.junit.Before;
-import org.junit.Test;
 
 import trees.BPlusTree;
 import util.BattingBPlusConverter;
 import util.BattingBPlusRecord;
+import util.BPlusRecord;
+import util.BPlusConverter;
+import util.Converter;
 
 import org.apache.hadoop.conf.Configuration;
 
@@ -18,32 +22,43 @@ public class HDFSBTreeTest {
      * @param args
      */
     public static void main(String[] args) throws IOException{
-        // TODO Auto-generated method stub
         if (args.length < 2) {
             System.err.println("not enough arguments");
             System.exit(-1);
         }
         Configuration conf = new Configuration();
-        BPlusTree<String, Long, BattingBPlusRecord> tree = 
-                new BPlusTree<String, Long, BattingBPlusRecord>(
+        BPlusTree<String, String, BPlusRecord<String, String>> tree = 
+                new BPlusTree<String, String, BPlusRecord<String, String>>(
                         7, 
-                        new BattingBPlusConverter(), 
+                        new BPlusConverter(), 
                         null, 
                         args[0],
                         args[1],
-                        conf);
-//        for (int i = 0; i < 100; ++i) {
-//            tree.insert(new BattingBPlusRecord("a" + i, new Long(i)));
-//        }
-//        tree.printTree();
-//        
+                        null);
+        for (int i = 0; i < 100; ++i) {
+            tree.insert(new BPlusRecord<String, String>("a" + i, "" + i));
+       }
+        for (int i = 0; i < 100; ++i) {
+            tree.insert(new BPlusRecord<String, String>("a" + i, "" + (i + 1)));
+        }
+        tree.printTree();
+        
         tree.syncToHdfs(args[1], conf);
-//        File deleteit = new File(args[0]);
-//        deleteit.delete();
+        File deleteit = new File(args[0]);
+        deleteit.delete();
         System.out.println();
         for (int i = 0; i < 100; ++i) {
-            System.out.print("search a" + i + " " + tree.search("a" + i));
+            System.out.println("search a" + i + " " + tree.search("a" + i));
+        }
+        for (int i = 0; i < 100; ++i) {
+            List<Long> results = tree.getRange("a" + i, "a" + i);
+            System.out.print("a" + i + ": ");
+            for (Long result: results) {
+                System.out.print(" " + result);
+            }
             System.out.println();
         }
+        
+        
     }
 }
---
+++ src/util/BPlusConverter.java
@@ -0,0 +1,62 @@
+package util;
+
+
+public class BPlusConverter implements Converter<String, String> {
+    @Override
+    public String bytesToKey( byte[] b )
+    {
+        return new String(b).trim();
+    }
+
+    @Override
+    public String bytesToRecord( byte[] b )
+    {
+        return new String(b).trim();
+    }
+    
+    @Override
+    public byte[] keyToBytes(String key)
+    {
+        int lengthDiff = 20 - key.length();
+        String byteKey = key;
+        if (lengthDiff < 0)
+            throw new IllegalArgumentException("Key value: " + key + "is too long!");
+        else if (lengthDiff > 0)
+        {
+            
+            for (int i = 0; i <= lengthDiff; i++)
+            {
+                byteKey += " ";
+            }
+        }            
+        return byteKey.getBytes();
+    }
+    @Override
+    public byte[] recordToBytes(String record)
+    {
+        int lengthDiff = 20 - record.length();
+        String byteRecord = record;
+        if (lengthDiff < 0)
+            throw new IllegalArgumentException("Record value: " + record + "is too long!");
+        else if (lengthDiff > 0)
+        {
+            
+            for (int i = 0; i <= lengthDiff; i++)
+            {
+                byteRecord += " ";
+            }
+        }            
+        return byteRecord.getBytes();
+    }
+    @Override
+    public int getKeyLength()
+    {
+        return 100;
+    }
+
+    @Override
+    public int getRecordLength()
+    {
+        return 100;
+    }
+}
